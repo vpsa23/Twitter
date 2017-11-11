a0d73c6b86f2e0046a6fa34227a75843185283c0
vpsa23 <vpsa23@hotmail.com>
2017-11-11 00:37:39 -0300

    Versión 0.0.2, Agregando funcion de deshabiliar boton

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,4 @@
-//Para Habilitar el comentario en el textarea.
-var btn = document.getElementById('btn');
-
+//Función del botón y creando div de comentarios 
 btn.addEventListener('click', function(){
 	var comments = document.getElementById('comment').value;
 	
@@ -22,6 +20,43 @@ btn.addEventListener('click', function(){
 	newTxt.appendChild(contElement);
 	text.appendChild(newTxt);
 })
+
+//Para Habilitar el comentario en el textarea.
+var btn = document.getElementById('btn');
+
+//Para deshabilitar el botón twittear y
+//Función que hace contar los carácteres de cuenta regresiva
+var cont = 0;
+numbers = 140;
+
+function counter(){
+	var menssage = document.getElementById('comment').value.length;
+	cont = menssage;
+
+	numbers = 140 - cont;
+	document.getElementById('contador').innerHTML = numbers;
+		if(cont >= 1){
+			document.getElementById('btn').disabled = false;
+			document.getElementById('btn').setAttribute('class', 'btn-hability');
+
+			if(cont >= 1 && cont < 120){
+				document.getElementById('contador').setAttribute('class', 'negro');
+			}
+
+			if (cont >= 120 && cont < 130) {
+				document.getElementById('contador').setAttribute('class', 'naranja');
+			}
+
+			if (cont >=130 && cont <= 140){
+				document.getElementById('contador').setAttribute('class', 'rojo');
+			}
+			if (cont == 0 || /^\s+$/.test(document.getElementById('comment').value) || cont > 140){
+				document.getElementById("btn").disabled = true;
+				document.getElementById('btn').setAttribute('class', 'btn');
+		}
+	}
+}
+
 
      
 
